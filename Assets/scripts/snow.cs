8dd11dd1b63d7d84e62d71815e3a0d9990d4e305
Canslp <canslp01@gmail.com>
2022-02-17 08:34:32 -0600

    weighted snow
    snow decays faster if it's moving slowly

--- Assets/scripts/snow.cs
+++ Assets/scripts/snow.cs
@@ -8,10 +8,12 @@ public class snow : MonoBehaviour
     public Transform focus;
     public Camera cam;
     public float speed = 2;
+    public float gravity = 0.05f;
     public int n = 100;
     public List<Transform> flakes;
     public List<float> flakeLife;
     public List<float> flakeWeight;
+    public List<Vector2> flakeV;
     public GameObject windParticle;
 
     public Vector2 ratio;
@@ -38,26 +40,36 @@ public class snow : MonoBehaviour
         for (int i = 0;i < flakes.Count;i++)
         {
             Vector2 w = (wind.getWind(flakes[i].position.x, flakes[i].position.y))*1f;
-            flakes[i].position += new Vector3(w.x, w.y,0)*speed*flakeWeight[i];
+            //flakes[i].position += new Vector3(w.x, w.y,0)*speed*flakeWeight[i];
+            flakeV[i] *= 0.95f;
+            flakeV[i] += new Vector2(w.x, w.y) * speed * flakeWeight[i];
+            flakes[i].position += new Vector3(flakeV[i].x, flakeV[i].y-gravity*flakeWeight[i],0);
             if (flakes[i].position.x >= focus.position.x + range.x * 0.5f)
             {
                 flakes[i].position = new Vector3(focus.position.x - range.x * 0.5f, focus.position.y + range.y * Random.Range(-0.5f, 0.5f), flakes[i].position.z);
+                flakeV[i] = new Vector2(0, 0);
             } else if (flakes[i].position.x <= focus.position.x - range.x * 0.5f)
             {
                 flakes[i].position = new Vector3(focus.position.x + range.x * 0.5f, focus.position.y + range.y * Random.Range(-0.5f, 0.5f), flakes[i].position.z);
+                flakeV[i] = new Vector2(0, 0);
             }
             if (flakes[i].position.y >= focus.position.y + range.y * 0.5f)
             {
                 flakes[i].position = new Vector3(focus.position.x + range.x * Random.Range(-0.5f, 0.5f), focus.position.y - range.y * 0.5f, flakes[i].position.z);
+                flakeV[i] = new Vector2(0, 0);
             } else if (flakes[i].position.y <= focus.position.y +- range.y * 0.5f)
             {
                 flakes[i].position = new Vector3(focus.position.x + range.x * Random.Range(-0.5f, 0.5f), focus.position.y + range.y * 0.5f, flakes[i].position.z);
+                flakeV[i] = new Vector2(0, 0);
             }
-            flakeLife[i] -= 0.0025f;
+            float decay = 0.025f/ (flakeV[i].magnitude+1);
+            
+            flakeLife[i] -= decay;
             if (flakeLife[i] <= 0)
             {
                 flakeLife[i] = 1;
                 flakes[i].position = new Vector3(focus.position.x + range.x * Random.Range(-0.5f, 0.5f), focus.position.y + range.y * Random.Range(-0.5f, 0.5f),3);
+                flakeV[i] = new Vector2(0, 0);
             }
             float s = Mathf.Pow((Mathf.Pow(1 / (1 + Mathf.Pow((flakeLife[i] *2) - 1, 2)), 2) - 0.25f) * (4f / 3f),0.5f);
             flakes[i].localScale = new Vector3(1, 1, 1) * 0.5f * s * cam.orthographicSize/20f;
@@ -76,5 +88,6 @@ public class snow : MonoBehaviour
         flakes.Add(flakeObj.transform);
         flakeLife.Add(Random.Range(0f,1f));
         flakeWeight.Add(Random.Range(0.9f, 1.1f));
+        flakeV.Add(new Vector2(0, 0));
     }
 }
