4a39e059ec9675ed0865037027d737b584f1ad74
Canslp <canslp01@gmail.com>
2022-02-21 21:15:52 -0600

    landing area

---
+++ Assets/scripts/landing_area.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class landing_area : MonoBehaviour
+{
+    public Transform trans;
+    public Transform balloonTrans;
+    public Transform playerTrans;
+    public bool occupied;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
