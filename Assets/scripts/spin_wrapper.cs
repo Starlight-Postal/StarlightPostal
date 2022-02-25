791acfa5c41872cd676eea29c08ba9a2c350b462
Canslp <canslp01@gmail.com>
2022-02-24 19:57:26 -0600

    it's a video game now

--- Assets/scripts/spin_wrapper.cs
+++ Assets/scripts/spin_wrapper.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class star : MonoBehaviour
+public class spin_wrapper : MonoBehaviour
 {
+    public Transform trans;
+    public float speed = 5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        trans = gameObject.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        trans.eulerAngles += new Vector3(0, 0, speed);
     }
 }
