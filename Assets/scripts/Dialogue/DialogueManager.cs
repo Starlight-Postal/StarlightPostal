6c5107a3ed4e19f1a5645072ac1b9ab72004fd72
Isaac-tesch <99272840+Isaac-tesch@users.noreply.github.com>
2022-03-01 08:07:36 -0600

    Implimented

---
+++ Assets/scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Ink.Runtime;
+using UnityEngine.EventSystems;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    [Header("Dialogue UI")]
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+
+    private Story currentStory;
+    public bool dialogueIsPlaying;
+
+    private static DialogueManager instance;
+
+
+    private void Awake() 
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Found more than one Dialogue Manager in the scene");
+        }
+        instance = this;
+    }
+
+    private void Start() 
+    {
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+    }
+
+    private void Update() 
+    {
+        if (!dialogueIsPlaying) 
+        {
+            return;
+        }
+        if (Input.GetKey("k"))
+        {
+            ContinueStory();
+        }
+
+    }
+
+    public void EnterDialogueMode(TextAsset inkJSON) 
+    {
+        currentStory = new Story(inkJSON.text);
+        dialogueIsPlaying = true;
+        dialoguePanel.SetActive(true);
+
+        ContinueStory();
+    }
+
+    private void ExitDialogueMode() 
+    {
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+        dialogueText.text = "";
+    }
+
+    private void ContinueStory()
+    {
+        if (currentStory.canContinue)
+        {
+            dialogueText.text = currentStory.Continue();
+        }
+        else
+        {
+            ExitDialogueMode();
+        }
+    }
+}
+
---
+++ Assets/scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Ink.Runtime;
+using UnityEngine.EventSystems;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [Header("Visual Cue")]
+    [SerializeField] private GameObject visualCue;
+    
+    [Header("Ink JSON")]
+    [SerializeField] private TextAsset inkJSON;
+
+    [Header("Dialogue UI")]
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+
+    private Story currentStory;
+    public bool dialogueIsPlaying;
+
+    private bool playerInRange;
+
+    private void Awake()
+    {
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+        playerInRange = false;
+        visualCue.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (playerInRange)
+        {
+            visualCue.SetActive(true);
+            if (Input.GetKey("j"))
+            {
+                Debug.Log(inkJSON.text);
+            }
+        }
+        else
+        {
+            visualCue.SetActive(false);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+}
