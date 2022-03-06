bd7c73e0084d396a8ce56866c3d04c5527ae4e0c
Canslp <canslp01@gmail.com>
2022-03-05 19:14:39 -0600

    Balloon functionality (#17)
    * balloon collision fixed lmao
    
    * anchor temp art
    
    * wind tweaks
    
    * balloon barriers (balloon won't get stuck in houses)
    
    * package dropoff
    
    * Update level 1.unity
    
    * basket :)
    
    * hmm
    
    * fixed player z fighting, fixed basket collision
    
    * balloon altitude exemption zones (so you can leave)
    
    * basket can't get stuck in balloon
    
    * Update level 1.unity
    
    * anchor sticks to things now :)
    
    * balloon landing
    
    you must be anchored to a landing zone and near the ground
    
    * auto land
    
    * i think a sprit 3 requirement is to have a "demo end" level
    
    * finished fixing merge conflicts
    
    Co-authored-by: YammyShep <jdbuelow@gmail.com>

--- Assets/scripts/anchor.cs
+++ Assets/scripts/anchor.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class anchor : MonoBehaviour
 {
+    public bool stuck;
+    public bool landed;
+    public GameObject target;
+    public Transform targetTrans;
+    public Vector3 tOff;
+    public Transform trans;
+    public Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        stuck = false;
+        trans = gameObject.GetComponent<Transform>();
+        landed = false;
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (stuck)
+        {
+            trans.position = targetTrans.position + tOff;
+            rb.bodyType = RigidbodyType2D.Static;
+            landed = (target.tag == "landing");
+        } else
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+            landed = false;
+        }
     }
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.gameObject.layer == 0)
+        {
+            //Debug.Log("stick");
+            stuck = true;
+            target = c.gameObject;
+            targetTrans = target.GetComponent<Transform>();
+            tOff = trans.position - targetTrans.position;
+        }
+    }
+    /*void OnCollisionExit2D(Collision2D c)
+    {
+        Debug.Log("unstick");
+    }*/
 }
--- Assets/scripts/balloon.cs
+++ Assets/scripts/balloon.cs
@@ -35,16 +35,29 @@ public class balloon : MonoBehaviour
 
 
     public wind wind;
-    public GameObject anchor;
+
+    public GameObject anchorObj;
+    public anchor anchor;
+    public Transform anchorTrans;
     public bool anchored;
     public float anchorD = 10;
     public float anchorRange = 10;
+    public Vector2 anchorStrength;
+    public Vector3 anchorOrg;
+    public bool landed = false;
 
+    public Rigidbody2D basket;
+    public Transform basketTrans;
+    public Collider2D basketCollider;
 
     public player player;
 
     int kiLMOUSE;
     int kiRMOUSE;
+
+    public Transform[] altExZones;
+
+    LineRenderer line;
 
     // Start is called before the first frame update
     void Start()
@@ -60,11 +73,22 @@ public class balloon : MonoBehaviour
         //wind = GameObject.Find("wind").GetComponent<wind>();
 
 
-        anchor = GameObject.Find("anchor");
-        anchor.SetActive(anchored);
+        anchorObj = GameObject.Find("anchor");
+        anchor = anchorObj.GetComponent<anchor>();
+        anchorObj.SetActive(anchored);
+        anchorTrans = anchorObj.GetComponent<Transform>();
         //anchored = true;
+        line = gameObject.GetComponent<LineRenderer>();
+        line.enabled=false;
 
-        Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), anchor.GetComponent<Collider2D>(), true);
+        basket = GameObject.Find("Basket").GetComponent<Rigidbody2D>();
+        basketCollider = GameObject.Find("Basket").GetComponent<Collider2D>();
+        basketTrans = GameObject.Find("Basket").GetComponent<Transform>();
+
+        //Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), anchor.GetComponent<Collider2D>(), true);
+        Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), basketCollider, true);
+
+        basket.centerOfMass = new Vector2(0, -1f);
     }
 
     // Update is called once per frame
@@ -73,8 +97,6 @@ public class balloon : MonoBehaviour
         UIUpdate();
         rb.velocity *= airFric;
 
-        //BuoyantControl(capped);
-        //ThrustControl();
         TargetControl();
 
         rb.velocity += wind.getWind(trans.position.x, trans.position.y)*windPower;
@@ -82,14 +104,16 @@ public class balloon : MonoBehaviour
 
         if (anchored)
         {
-            Transform aTrans = anchor.GetComponent<Transform>();
+            
             Rigidbody2D anchorRB = anchor.GetComponent<Rigidbody2D>();
-            float d = Vector3.Distance(aTrans.position, trans.position);
+            float d = Vector3.Distance(anchorTrans.position, trans.position+anchorOrg);
             if (d > anchorD)
             {
-                float a = Mathf.Atan2(trans.position.x - aTrans.position.x, trans.position.y - aTrans.position.y);
-                rb.velocity += new Vector2(Mathf.Sin(a) * (d - anchorD) * -0.2f, Mathf.Cos(a) * (d - anchorD) * -0.2f);
-                anchorRB.velocity += new Vector2(Mathf.Sin(a) * (d - anchorD) * 0.05f, Mathf.Cos(a) * (d - anchorD) * 0.05f);
+                float a = Mathf.Atan2(trans.position.x + anchorOrg.x - anchorTrans.position.x, trans.position.y + anchorOrg.y - anchorTrans.position.y);
+                rb.velocity += new Vector2(Mathf.Sin(a) * (d - anchorD) * -anchorStrength.x, Mathf.Cos(a) * (d - anchorD) * -anchorStrength.x);
+                if (!anchor.stuck) {
+                    anchorRB.velocity += new Vector2(Mathf.Sin(a) * (d - anchorD) * anchorStrength.y, Mathf.Cos(a) * (d - anchorD) * anchorStrength.y);
+                }
             }
 
             if (player.inBalloon)
@@ -102,12 +126,35 @@ public class balloon : MonoBehaviour
                 {
                     //Debug.Log("retract");
                     anchored = false;
-                    anchor.SetActive(false);
+                    anchorObj.SetActive(false);
+                    anchor.stuck = false;
+                    anchor.landed = false;
                 }
             } else
             {
                 anchorD += (2.5f - anchorD) * 0.01f;
                 //volume *= 0.999f;
+            }
+
+            line.enabled = true;
+            line.SetPosition(0, trans.position + anchorOrg);
+            line.SetPosition(1, anchorTrans.position+new Vector3(0,0.25f,0));
+
+            if (anchor.landed)
+            {
+                if (basketTrans.position.y > anchor.targetTrans.position.y && basketTrans.position.y - anchor.targetTrans.position.y < 1.5f)
+                {
+                    landed = true;
+                } else
+                {
+                    landed = false;
+                }
+                
+                th += ((anchor.targetTrans.position.y+3.5f) - th) * 0.005f;
+                //anchorD += (1 - anchorD) * 0.01f;
+            } else
+            {
+                landed = false;
             }
         }
         else
@@ -119,88 +166,20 @@ public class balloon : MonoBehaviour
                     //Debug.Log("throw");
                     anchored = true;
                     anchorD = anchorRange;
-                    anchor.GetComponent<Transform>().position = trans.position + new Vector3(0, -3, 0);
-                    anchor.SetActive(true);
+                    anchorObj.GetComponent<Transform>().position = trans.position + new Vector3(0, -3, 0);
+                    anchorObj.SetActive(true);
+                    anchor.stuck = false;
+                    anchor.landed = false;
                     //anchor.GetComponent<SpringJoint2D>().distance = 10;
                 }
             }
+            line.enabled = false;
+            landed = false;
         }
+
 
     }
 
-    /*void BuoyantControl(bool capped)
-    {
-        sprite.color = new Color(1, 0.3f, 0.3f);
-        if (player.inBalloon)
-        {
-            if (Input.GetKey("up") || Input.GetKey("w"))
-            {
-                volume += fillRate;
-                sprite.color = new Color(1, 0.5f, 0.5f);
-
-            }
-            if (Input.GetKey("down") || Input.GetKey("s"))
-            {
-                volume -= fillRate;
-                sprite.color = new Color(1, 0f, 0f);
-            }
-            if (capped)
-            {
-                if (volume > volCap)
-                {
-                    volume += (volCap - volume) * 0.1f;
-                }
-                if (volume < 0)
-                {
-                    volume *= 0.9f;
-                }
-            }
-            lean *= 0.75f;
-            if (Input.GetKey("right") || Input.GetKey("d"))
-            {
-                lean += leanPower;
-            }
-            if (Input.GetKey("left") || Input.GetKey("a"))
-            {
-                lean -= leanPower;
-            }
-        }
-        bouyancy = getBouyancy(trans.position.y);
-
-        rb.velocity += new Vector2(0, bouyancy * volume);
-        rb.velocity += new Vector2(0, -gravity * weight);
-        rb.velocity += new Vector2(lean, 0);
-    }
-
-    void ThrustControl()
-    {
-        sprite.color = new Color(1, 0.3f, 0.3f);
-        rb.velocity += new Vector2(0, -0.025f);
-        if (player.inBalloon)
-        {
-            if (Input.GetKey("up"))
-            {
-                rb.velocity += new Vector2(0, 0.1f);
-                sprite.color = new Color(1, 0.5f, 0.5f);
-
-            }
-            if (Input.GetKey("down"))
-            {
-                rb.velocity += new Vector2(0, -0.1f);
-                sprite.color = new Color(1, 0f, 0f);
-            }
-            lean *= 0.75f;
-            if (Input.GetKey("right"))
-            {
-                lean += leanPower; ;
-            }
-            if (Input.GetKey("left"))
-            {
-                lean -= leanPower;
-            }
-        }
-        rb.velocity += new Vector2(lean, 0);
-    }*/
 
     void TargetControl()
     {
@@ -234,7 +213,19 @@ public class balloon : MonoBehaviour
         }
         if (th > heightCap)
         {
-            th += (heightCap - th) * 0.1f;
+            bool ex = false;
+            for(int i = 0;i < altExZones.Length;i++)
+            {
+                if(Mathf.Abs(trans.position.x-altExZones[i].position.x)<=altExZones[i].localScale.x/2f&& Mathf.Abs(trans.position.y - altExZones[i].position.y) <= altExZones[i].localScale.y / 2f)
+                {
+                    ex = true;
+                    break;
+                }
+            }
+            if (!ex)
+            {
+                th += (heightCap - th) * 0.1f;
+            }
         }
 
         targetHeight = th / weight;
@@ -245,11 +236,6 @@ public class balloon : MonoBehaviour
         //Debug.Log(targetHeight);
     }
 
-    /*float getBouyancy(float y)
-    {
-        //return atmPressure / (y + 1);
-        return (40 - y) * atmPressure;
-    }*/
     void UIUpdate()
     {
         if (Input.GetMouseButton(0))
@@ -284,6 +270,17 @@ public class balloon : MonoBehaviour
         }
     }
 
+    float toAngle(float a,float b,float amt)
+    {
+        float xa = Mathf.Sin(a);
+        float xb = Mathf.Sin(b);
+        float ya = Mathf.Cos(a);
+        float yb = Mathf.Cos(b);
+        float x = xa * (1 - amt) + xb * amt;
+        float y = ya * (1 - amt) + yb * amt;
+        return Mathf.Atan2(y,x);
+    }
+
     [ConsoleMethod("balloon.skin", "change the balloon skin")]
     public static void ChangeBalloonSkin(string name) {
         var spriteRender = GameObject.Find("balloon").GetComponent<SpriteRenderer>();
@@ -316,4 +313,5 @@ public class balloon : MonoBehaviour
         }
         Debug.Log(output);
     }
+    
 }
