46df830a7d021381ea8929d886865c1cdeafc49a
Isaac-tesch <99272840+Isaac-tesch@users.noreply.github.com>
2022-03-05 20:32:20 -0600

    no more ghosts
    Got rid of the duplicate. With NPC's will need to make new code maybe for each indivitual NPC. Or until I finagle more.

--- Assets/scripts/DialogueTrigger.cs
+++ Assets/scripts/DialogueTrigger.cs
@@ -20,12 +20,20 @@ public class DialogueTrigger : MonoBehaviour
     private Button ChatButton;
     private Label Script;
     private int counter;
+    [SerializeField] private VisualElement rve;
 
-    private VisualElement rve;
+    //bool's for players they talked too
+    private bool firstNPC = false;
 
-
-    private void Update()
+    private void Start()
     {
+        playerInRange = false;
+        visualCue.SetActive(false);
+        player = GameObject.Find("player").GetComponent<player>();
+
+    }
+
+    /*
         if (Input.GetKeyDown("i") && playerInRange)
         {
             inMenu = true;
@@ -33,36 +41,49 @@ public class DialogueTrigger : MonoBehaviour
             rve.visible = inMenu;
 
         }
-        if (playerInRange == false)
-        {
-            inMenu = false;
-            rve.visible = inMenu;
-        }
+        */
+    /*     {
+             if (playerInRange == false) 
+             {
+                 counter = 0;
+                 nextLine();
+             }
+         }
+
+             if (playerInRange)
+            {
+                // dialoguePanel.SetActive(true);
+
+                visualCue.SetActive(true);
+            }
+     */
+
+    private void Update()
+    {
         if (playerInRange)
         {
-            // dialoguePanel.SetActive(true);
-
             visualCue.SetActive(true);
+            if (Input.GetKeyDown("i"))
+            {
+                counter = 0;
+                inMenu = true;
+                rve.visible = inMenu;
+            }
         }
         else
         {
             visualCue.SetActive(false);
-
+            if (playerInRange == false)
+            {
+                counter = 0;
+                nextLine();
+            }
         }
+    
         rve.visible = inMenu;
     }
-
-
-    
     private void OnEnable()
     {
-        playerInRange = false;
-        visualCue.SetActive(false);
-
-        player = GameObject.Find("player").GetComponent<player>();
-
-
-
         rve = GetComponent<UIDocument>().rootVisualElement;
         ChatButton = rve.Q<Button>("ChatButton");
         Script = rve.Q<Label>("chatLabel");
@@ -71,18 +92,25 @@ public class DialogueTrigger : MonoBehaviour
         rve.visible = false;
 
     }
-    
+
+
     private void nextLine()
     {
         if (counter == 1) { Script.text = "I hate to say this, but we are running behind on mail"; }
         else if (counter == 2) { Script.text = "we would like for you to start right away"; }
-        else if (counter == 3) { Script.text = "Hurry, up and fly up to the first town with the mail"; }
-        else 
+        else if (counter == 3) 
+        { 
+            Script.text = "Hurry, up and fly up to the first town with the mail";
+            firstNPC = true;
+        }
+        else
         {
+            Script.text = "Hello,____ glad to see you getting here nice and safly.";
             inMenu = false;
             rve.visible = inMenu;
             counter = 0;
         }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
@@ -101,3 +129,5 @@ public class DialogueTrigger : MonoBehaviour
         }
     }
 }
+
+
