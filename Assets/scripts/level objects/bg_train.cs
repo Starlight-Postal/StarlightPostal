18874d82bfd512c8cb0f93b6ffbfa3566c31cf7f
Canslp <canslp01@gmail.com>
2022-03-07 19:28:03 -0600

    choo choo 2

--- Assets/scripts/level objects/bg_train.cs
+++ Assets/scripts/level objects/bg_train.cs
@@ -7,7 +7,8 @@ public class bg_train : MonoBehaviour
     Transform trans;
     public float x1;
     public float x2;
-    public float speed = 0.001f;
+    public float speed = 0.005f;
+    public float vel = 0.1f;
     float x;
     // Start is called before the first frame update
     void Start()
@@ -19,7 +20,14 @@ public class bg_train : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        x += (x2 - x) * speed;
+        float xv = (x2 - x) * speed;
+        if (xv <= vel)
+        {
+            x += xv;
+        } else
+        {
+            x += vel;
+        }
         trans.position = new Vector3(x,trans.position.y, trans.position.z);
     }
 }
