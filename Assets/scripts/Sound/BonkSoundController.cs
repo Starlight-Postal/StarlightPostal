57dd333a8d3edd3bd87917cffb8bcbfe9e7cc101
YammyShep <jdbuelow@gmail.com>
2022-03-28 19:35:29 -0500

    made the bonks use a heavy, medium and light bonk sound

--- Assets/scripts/Sound/BonkSoundController.cs
+++ Assets/scripts/Sound/BonkSoundController.cs
@@ -5,31 +5,52 @@ using UnityEngine.Audio;
 
 public class BonkSoundController : MonoBehaviour {
 
-    private static float BONK_THRESHOLD = 3;
+    private static float BONK_HEAVY_THRESHOLD = 8;
+    private static float BONK_MEDIUM_THRESHOLD = 5;
+    private static float BONK_LIGHT_THRESHOLD = 0;
 
     public AudioMixerGroup mixer;
 
     private AudioSource source;
-    private AudioClip[] clips;
+    private AudioClip[] heavyClips;
+    private AudioClip[] mediumClips;
+    private AudioClip[] lightClips;
 
     void Start() {
+
         source = gameObject.AddComponent<AudioSource>();
         source.outputAudioMixerGroup = mixer;
 
-        clips = Resources.LoadAll<AudioClip>("audio/SFX/balloon/bonk");
+        heavyClips = Resources.LoadAll<AudioClip>("audio/SFX/balloon/bonk/heavy");
+        mediumClips = Resources.LoadAll<AudioClip>("audio/SFX/balloon/bonk/medium");
+        lightClips = Resources.LoadAll<AudioClip>("audio/SFX/balloon/bonk/light");
+
     }
 
-    private void Play() {
-        source.clip = clips[Random.Range(0,clips.Length - 1)];
+    private void Play(AudioClip[] clipArr) {
+
+        source.clip = clipArr[Random.Range(0,clipArr.Length - 1)];
         source.Play();
+
     }
 
     public void Bonk(float magnitude) {
 
-        if (magnitude >= BONK_THRESHOLD) {
-            Play();
+        if (magnitude < BONK_LIGHT_THRESHOLD) {
+            return;
         }
-        
+
+        if (magnitude >= BONK_HEAVY_THRESHOLD) {
+            // Heavy bonkage
+            Play(heavyClips);
+        } else if (magnitude >= BONK_MEDIUM_THRESHOLD) {
+            // Medium bonkage
+            Play(mediumClips);
+        } else if (magnitude >= BONK_LIGHT_THRESHOLD) {
+            // Light bonkage
+            Play(lightClips);
+        }
+
     }
 
 }
