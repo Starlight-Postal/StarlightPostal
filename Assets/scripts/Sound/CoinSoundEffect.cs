291e6d60ed5821c8b796d563dcdad508d95ba26a
YammyShep <jdbuelow@gmail.com>
2022-03-29 19:50:41 -0500

    dynamically allocating audio sources for tone sounds bc unity is dum

--- Assets/scripts/Sound/CoinSoundEffect.cs
+++ Assets/scripts/Sound/CoinSoundEffect.cs
@@ -12,7 +12,8 @@ public class CoinSoundEffect : MonoBehaviour {
     private AudioSource source;
     private AudioClip[] clips;
 
-    private AudioSource toneSource;
+    private List<AudioSource> toneSources = new List<AudioSource>();
+    private AudioClip toneClip;
 
     // Start is called before the first frame update
     void Start() {
@@ -21,27 +22,40 @@ public class CoinSoundEffect : MonoBehaviour {
 
         clips = Resources.LoadAll<AudioClip>("audio/SFX/coin");
 
-        toneSource = gameObject.AddComponent<AudioSource>();
-        toneSource.clip = Resources.LoadAll<AudioClip>("audio/SFX/cointone")[0];
-        toneSource.outputAudioMixerGroup = mixer;
+        toneClip = Resources.LoadAll<AudioClip>("audio/SFX/cointone")[0];
 
         instance = this;
     }
 
-    // Update is called once per frame
-    void Update() {
-        
+    private void Cleanup() {
+        toneSources.RemoveAll(item => item == null);
+        toneSources.ForEach(toneSource => {
+            if (!toneSource.isPlaying) {
+                Destroy(toneSource);
+            }
+        });
     }
 
     private void Play() {
+
         var clip = clips[Random.Range(0,clips.Length - 1)];
         source.PlayOneShot(clip, 1);
 
+        var toneSource = gameObject.AddComponent<AudioSource>();
+        toneSources.Add(toneSource);
+        toneSource.outputAudioMixerGroup = mixer;
+        toneSource.clip = toneClip;
+
+        //                    C  D  E  G  A
         var notes = new int[]{0, 2, 4, 7, 9};
         var note = notes[Random.Range(0,notes.Length - 1)];
 
+        //                      input clip is D
         toneSource.pitch = Mathf.Pow(2, (note-2)/12.0f);
         toneSource.Play();
+
+        Cleanup();
+
     }
 
     public static void CoinCollect() {
