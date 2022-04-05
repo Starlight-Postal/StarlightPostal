0757bb24bceb01dac9c862d7b4d7d8c6455cf8e9
yammyshep <jdbuelow@gmail.com>
2022-04-05 10:20:50 -0500

    made background scaler scale to any resolution. will always cover the enire screen, though has weird margin errors

--- Assets/scripts/player/BgFillScreenHorizontally.cs
+++ Assets/scripts/player/BgFillScreenHorizontally.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class BgFillScreenHorizontally : MonoBehaviour {
 
+    private Vector2 lastScreenSize;
+    private Vector3 initScale;
+
     void Start() {
-        
-        var scale = gameObject.transform.localScale;
+        lastScreenSize = new Vector2(Screen.width, Screen.height);
+        initScale = gameObject.transform.localScale;
+        Resize();
+    }
+
+    void Update() {
+        var screenSize = new Vector2(Screen.width, Screen.height);
+        if (lastScreenSize != screenSize) {
+            Resize();
+            lastScreenSize = screenSize;
+        }
+    }
+
+    private void Resize() {
         var cam = Camera.main;
         
-        var x = (cam.aspect / ((float)16/9)) * scale.x;
-        var y = (cam.aspect / ((float)16/9)) * scale.y;
+        float x, y;
+        if (cam.aspect >= (float)16/9) {
+            x = (cam.aspect / ((float)16/9)) * initScale.x;
+            y = (cam.aspect / ((float)16/9)) * initScale.y;
+        } else {            
+            y = ((1 / cam.aspect) * ((float)16/9)) * initScale.y;
+            x = ((1 / cam.aspect) * ((float)16/9)) * initScale.x;
+        }
 
-        gameObject.transform.localScale = new Vector3 (x, y, scale.z);
-
+        gameObject.transform.localScale = new Vector3 (x, y, initScale.z);
     }
 
 }
