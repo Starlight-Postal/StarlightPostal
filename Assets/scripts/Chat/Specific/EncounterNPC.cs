fa52e3cc6984ea8e26881702fe26b296e2f67274
YammyShep <jdbuelow@gmail.com>
2022-04-15 22:23:27 -0500

    reimplemented package visual/sfx

--- Assets/scripts/Chat/Specific/EncounterNPC.cs
+++ Assets/scripts/Chat/Specific/EncounterNPC.cs
@@ -8,8 +8,16 @@ public class EncounterNPC : Conversation
     public string[] loopScript;
     public bool deliveredTo = false;
 
+    public GameObject package;
+    public AudioSource sound;
+
     public override void OnConversationEnd()
     {
+        if (!deliveredTo)
+        {
+            package.SetActive(true);
+            sound.Play();
+        }
         deliveredTo = true;
         script = loopScript;
         GameObject.FindObjectsOfType<PostOfficeClerk>()[0].phase = MailPhase.DELIVERED;
