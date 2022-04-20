4721f5bb65f61e224f258f667e5eb9e614d06ffc
YammyShep <jdbuelow@gmail.com>
2022-04-19 21:26:24 -0500

    bob (real)

--- Assets/scripts/BuildScripts/StarlightBuilder.cs
+++ Assets/scripts/BuildScripts/StarlightBuilder.cs
@@ -13,7 +13,7 @@ public static class StarlightBuilder
     private static string EXEC_NAME = "StarlightPostal";
     private static string FOLDER_PREFIX = "StarlightPostal";
 
-    [MenuItem("StarlightBuilder/> Build All")]
+    [MenuItem("Bob the Builder/BUILD IT ALL!")]
     public static void BuildAll()
     {
         Windows();
@@ -21,40 +21,37 @@ public static class StarlightBuilder
         MacOS();
         WebGL();
         Android();
-
-        // zip/tar
-        // calc sums
     }
 
-    [MenuItem("StarlightBuilder/Windows")]
+    [MenuItem("Bob the Builder/Windows")]
     public static void Windows()
     {
         SetupVariables();
         BuildPipeline.BuildPlayer(GetScenes(),buildLocation + "/" + FOLDER_PREFIX + "-windows-x86_64/" + EXEC_NAME + ".exe",BuildTarget.StandaloneWindows64,BuildOptions.None);
     }
 
-    [MenuItem("StarlightBuilder/Linux")]
+    [MenuItem("Bob the Builder/Linux")]
     public static void Linux()
     {
         SetupVariables();
         BuildPipeline.BuildPlayer(GetScenes(),buildLocation + "/" + FOLDER_PREFIX + "-linux-x86_64/" + EXEC_NAME,BuildTarget.StandaloneLinux64,BuildOptions.None);
     }
 
-    [MenuItem("StarlightBuilder/MacOS")]
+    [MenuItem("Bob the Builder/MacOS")]
     public static void MacOS()
     {
         SetupVariables();
         BuildPipeline.BuildPlayer(GetScenes(),buildLocation + "/" + FOLDER_PREFIX + "-macos/" + EXEC_NAME + ".app",BuildTarget.StandaloneOSX,BuildOptions.None);
     }
 
-    [MenuItem("StarlightBuilder/WebGL")]
+    [MenuItem("Bob the Builder/WebGL")]
     public static void WebGL()
     {
         SetupVariables();
         BuildPipeline.BuildPlayer(GetScenes(),buildLocation + "/" + FOLDER_PREFIX + "-webgl",BuildTarget.WebGL,BuildOptions.None);
     }
 
-    [MenuItem("StarlightBuilder/Android")]
+    [MenuItem("Bob the Builder/Android")]
     public static void Android()
     {
         SetupVariables();
