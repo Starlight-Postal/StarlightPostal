b5d8da331018c3775326cec4db953a8394b22c17
YammyShep <jdbuelow@gmail.com>
2023-10-28 17:54:24 -0500

    Joystick rebinding support and reel implemented

--- Assets/scripts/MdevDlc/JoystickInputHandler.cs
+++ Assets/scripts/MdevDlc/JoystickInputHandler.cs
@@ -5,24 +5,33 @@ using UnityEngine.InputSystem;
 
 public class JoystickInputHandler : MonoBehaviour
 {
-
-    [SerializeField] private float horizontal, vertical;
+    [SerializeField] private KeyCode coinButton = KeyCode.JoystickButton6;
+    [SerializeField] private KeyCode interactButton = KeyCode.JoystickButton4;
+    [SerializeField] private KeyCode anchorButton = KeyCode.JoystickButton5;
+    [SerializeField] private KeyCode pauseButton = KeyCode.JoystickButton7;
+    [SerializeField] private KeyCode reelButton = KeyCode.JoystickButton0;
 
     private player m_player;
     private PauseMenuBehaviour m_pause;
+
+    private float prevHorz, prevVert;
+    private bool prevReel = false;
     
     // Start is called before the first frame update
     void Start()
     {
         m_player = FindObjectOfType<player>();
         m_pause = FindObjectOfType<PauseMenuBehaviour>();
+
+        prevHorz = 0.0f;
+        prevVert = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        float horz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
 
         if (m_pause && m_pause.inMenu)
         {
@@ -34,29 +43,37 @@ public class JoystickInputHandler : MonoBehaviour
             {
                 Debug.LogError("Player script invalid!");
             }
-            
-            gameObject.BroadcastMessage(m_player.inBalloon ? "OnLean" : "OnMove", horizontal);
 
-            if (m_player.inBalloon)
+            if (horz != prevHorz)
             {
-                gameObject.BroadcastMessage("OnBurnVent", vertical);
+                gameObject.BroadcastMessage(m_player.inBalloon ? "OnLean" : "OnMove", horz);
+            }
+
+            if (m_player.inBalloon && vert != prevVert)
+            {
+                gameObject.BroadcastMessage("OnBurnVent", vert);
             }
             else
             {
-                if (vertical < -0.5f)
+                if (vert < -0.5f)
                 {
                     gameObject.BroadcastMessage("OnPlatformDrop");
                 }
             }
-        
-            //TODO: reel
 
-            if (Input.GetKeyDown(KeyCode.JoystickButton2))
+            bool reel = Input.GetKey(reelButton);
+            if (reel != prevReel)
+            {
+                gameObject.BroadcastMessage("OnReel", reel ? 1.0f : 0.0f);
+            }
+            prevReel = reel;
+
+            if (Input.GetKeyDown(interactButton))
             {
                 gameObject.BroadcastMessage(m_player.inBalloon ? "OnLeaveBalloon" : "OnInterract");
             }
 
-            if (Input.GetKeyDown(KeyCode.JoystickButton3))
+            if (Input.GetKeyDown(anchorButton))
             {
                 if (m_player.inBalloon)
                 {
@@ -73,9 +90,12 @@ public class JoystickInputHandler : MonoBehaviour
         {
         }
         
-        if (Input.GetKeyDown(KeyCode.JoystickButton1))
+        if (Input.GetKeyDown(pauseButton))
         {
             gameObject.BroadcastMessage("OnPauseGame");
         }
+
+        prevHorz = horz;
+        prevVert = vert;
     }
 }
