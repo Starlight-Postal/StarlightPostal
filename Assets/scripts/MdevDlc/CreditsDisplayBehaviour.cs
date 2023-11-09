7607e46a4ab8c346224429b26d62ab78e23dcfae
YammyShep <jdbuelow@gmail.com>
2023-11-08 20:36:47 -0600

    Funny immersion breaking font

--- Assets/scripts/MdevDlc/CreditsDisplayBehaviour.cs
+++ Assets/scripts/MdevDlc/CreditsDisplayBehaviour.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -9,11 +11,14 @@ public class CreditsDisplayBehaviour : MonoBehaviour
 
     private CreditAccumulator creditAcc;
 
+    private bool bInsert = false;
+
     private void OnEnable()
     {
         var rve = GetComponent<UIDocument>().rootVisualElement;
 
         creditsLabel = rve.Q<Label>("credit-counter-label");
+        StartCoroutine(FlashInsertCorroutine());
     }
 
     private void Start()
@@ -31,6 +36,23 @@ public class CreditsDisplayBehaviour : MonoBehaviour
     public void UpdateCreditLabel()
     {
         int c = creditAcc.GetCredits();
-        creditsLabel.text = $"Credits: {c}";
+        
+        if (bInsert && c <= 0)
+        {
+            creditsLabel.text = $"Insert Credits";
+        }
+        else
+        {
+            creditsLabel.text = $"Credits: {c}";
+        }
+    }
+
+    private IEnumerator FlashInsertCorroutine()
+    {
+        while (true)
+        {
+            bInsert = !bInsert;
+            yield return new WaitForSecondsRealtime(2.0f);
+        }
     }
 }
